668d201d75bd4c20563fba013b7bf57d847dca7a
Bashir Muhammed <bashmufol@gmail.com>
2025-06-25 15:24:45 +0100

    My first commit

---
+++ src/main/java/com/bash/login_sec/LoginSecApplication.java
@@ -0,0 +1,13 @@
+package com.bash.login_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/login_sec/LoginSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.login_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
